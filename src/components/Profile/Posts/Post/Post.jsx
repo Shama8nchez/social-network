fb317174f5b9b85795fc02c0889fadb06e4ab019
Shama8nchez <shamanouski@gmail.com>
2023-08-07 16:54:53 +0300

    feat: add props to post

--- src/components/Profile/Posts/Post/Post.jsx
+++ src/components/Profile/Posts/Post/Post.jsx
@@ -1,12 +1,12 @@
 import classes from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
   return (
       <div>
         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZdblbGSMjQAUhh058K1lUYPSVC2dWBNk1EQ&usqp=CAU' alt='ava' className={classes.ava} />
-        <span>Hello</span>
+        <span>{props.post}</span>
         <br />
-        <span>Likes</span> 5
+        <span>Likes</span> {props.likesCount}
       </div>
   )
 }
--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -8,10 +8,10 @@ const Posts = () => {
       <textarea name="" id="" cols="60" rows="10"></textarea>
       <br />
       <button>Add post</button>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post post="Hello!" likesCount="78" />
+      <Post post="yoyoy!" likesCount="26" />
+      <Post post="aliluya!" likesCount="11" />
+      <Post post="Let's go!" likesCount="43" />
     </div>
   )
 }
