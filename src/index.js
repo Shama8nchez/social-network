8b31bc7d185ffd63479b23df24ebc280b3760e1c
Shama8nchez <shamanouski@gmail.com>
2023-08-07 20:45:52 +0300

    feat: add state

--- src/index.js
+++ src/index.js
@@ -4,67 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import state from './redux/state';
 
-const postsDB = [
-  {
-    id: 1,
-    post: "Hello!",
-    likesCount: 78
-  },
-  {
-    id: 2,
-    post: "yoyoy!!",
-    likesCount: 26
-  },
-  {
-    id: 3,
-    post: "aliluya!",
-    likesCount: 11
-  },
-  {
-    id: 4,
-    post: "Let's go!",
-    likesCount: 43
-  },
-];
-
-const dialogsDB = [
-  {
-    id: 1,
-    userName: 'Andrey'
-  },
-  {
-    id: 2,
-    userName: 'Anna'
-  },
-  {
-    id: 3,
-    userName: 'Eugeniy'
-  },
-  {
-    id: 4,
-    userName: 'Fedor'
-  },
-  {
-    id: 5,
-    userName: 'Denis'
-  },
-];
-
-const messages = [
-  {
-    id: 1,
-    message: 'Hello!',
-  },
-  {
-    id: 2,
-    message: 'How are you?',
-  },
-  {
-    id: 3,
-    message: 'Have fun!',
-  },
-]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/redux/state.js
@@ -0,0 +1,22 @@
+const state = {
+  postsDB: [
+    { id: 1, post: "Hello!", likesCount: 78 },
+    { id: 2, post: "yoyoy!!", likesCount: 26 },
+    { id: 3, post: "aliluya!", likesCount: 11 },
+    { id: 4, post: "Let's go!", likesCount: 43 },
+  ],
+  dialogsDB: [
+    { id: 1, userName: 'Andrey' },
+    { id: 2, userName: 'Anna' },
+    { id: 3, userName: 'Eugeniy' },
+    { id: 4, userName: 'Fedor' },
+    { id: 5, userName: 'Denis' },
+  ],
+  messages: [
+    { id: 1, message: 'Hello!' },
+    { id: 2, message: 'How are you?' },
+    { id: 3, message: 'Have fun!' },
+  ]
+}
+
+export default state;
