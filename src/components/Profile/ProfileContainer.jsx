d445f876b2348a5c3f396ddb289d92d8f88d2947
Shama8nchez <shamanouski@gmail.com>
2023-08-15 19:02:17 +0300

    refactor: move withRouter to utils

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,29 +1,9 @@
 import React from "react";
 import Profile from "./Profile";
 import axios from 'axios';
-import {
-  useLocation,
-  useNavigate,
-  useParams,
-} from "react-router-dom";
+import withRouter from "../../utils/withRouter";
 import { connect } from "react-redux";
 import { getUserAC } from "../../redux/usersReducer";
-
-function withRouter(Component) {
-  function ComponentWithRouterProp(props) {
-    let location = useLocation();
-    let navigate = useNavigate();
-    let params = useParams();
-    return (
-      <Component
-        {...props}
-        router={{ location, navigate, params }}
-      />
-    );
-  }
-
-  return ComponentWithRouterProp;
-}
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
