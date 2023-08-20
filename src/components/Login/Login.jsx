14a59e63f0e72026b89a651fafa459c91db1289a
Shama8nchez <shamanouski@gmail.com>
2023-08-20 13:20:48 +0300

    feat: add login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,63 @@
+import { Formik, Field, Form } from 'formik';
+
 const Login = () => {
+  return (
+    <div>
+      <h1>Login:</h1>
+      <LoginForm />
+    </div>
+    
+  )
+}
+
+const LoginForm = () => {
   
   return (
-    <h1>Login</h1>
+    <Formik
+      initialValues={{
+        Login: '',
+        Password: '',
+        RememeberMe: false,
+      }}
+      validate={values => {
+        const errors = {};
+
+        if (!values.Login) {
+          errors.Login = 'Field is empty';
+        } else if (!values.Password) {
+          errors.Password = 'Field is empty';
+        }
+
+        return errors;
+      }}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {({ values, errors, touched, handleChange, handleReset }) => (
+        <Form>
+          <div>
+            <label htmlFor="Login">Login</label>
+            <Field id="Login" name="Login" placeholder="Login" onChange={handleChange} value={values.Login} />
+            {errors.Login && touched.Login && errors.Login}
+          </div>
+
+          <div>
+            <label htmlFor="Password">Password</label>
+            <Field id="Password" name="Password" placeholder="Password" type="password" onChange={handleChange} value={values.Password} />
+            {errors.Password && touched.Password && errors.Password}
+          </div>
+
+          <div>
+            <label htmlFor="RememeberMe">Remember me</label>
+            <Field id="RememeberMe" name="RememeberMe" type="checkbox" />
+          </div>
+
+          <button type="submit" onClick={handleReset}>LOGIN</button>
+        </Form>
+      )}
+      
+    </Formik>
   )
 }
 
