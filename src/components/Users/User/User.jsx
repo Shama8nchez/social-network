703546e7ab0c32cc21b657460bf33b66a9c43b68
Shama8nchez <shamanouski@gmail.com>
2023-08-27 22:17:16 +0300

    feat: update user component

--- src/components/Users/User/User.jsx
+++ src/components/Users/User/User.jsx
@@ -1,19 +1,23 @@
 import { NavLink } from 'react-router-dom';
 import classes from './User.module.css'
+import unknown from '../../../assets/unknown.png'
 
 const User = (props) => {
   return (
     <div className={classes.user}>
       <div className={classes.userInfo}>
+        
         <NavLink to={`/${props.id}`} className={classes.userLink}>
           {props.user.name}
+          <br />
+          <img src={props.user.photos.small ? props.user.photos.small : unknown} alt='user' className={classes.avatar} />
         </NavLink>
         
         <div>
           <div className={classes.userStatus}>Status: {props.user.status}</div>
         </div>
       </div>
-      <button className='userButton'
+      <button className={classes.userButton}
         onClick={props.user.followed ? () => props.unfollowUser(props.user.id) : () => props.followUser(props.user.id)}
         disabled={props.followingProgress.some(id => id === props.user.id)}
       >{props.user.followed ? 'Unfollow' : 'Follow'}</button>
